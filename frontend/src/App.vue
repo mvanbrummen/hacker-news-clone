0f40d24a43b86e93e5080746458c95d8b46f7fb6
Michael Van Brummen <michaelvanbrummen@gmail.com>
2022-01-04 12:54:25 +1000

    Added basic styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,47 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/" class="logo">HackerNews</router-link> |
+    <router-link to="/about" class="nav-link">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+
+  font-family: Verdana, Geneva, sans-serif;
+  font-size: 10pt;
+  color: #828282;
+  background-color: #f6f6ef;
+  height: 100vh;
+  width: 80vw;
+  margin: auto;
 }
 
 #nav {
-  padding: 30px;
+  padding: 2px;
+  background-color: #ff6600;
+  height: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  color: #000000;
+  text-decoration: none;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.logo {
+  color: #000000;
+  margin-right: 5px;
+  font-family: Verdana, Geneva, sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+  color: #222222;
+}
+
+#nav .nav-link.router-link-exact-active {
+  color: #fff;
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <items></items>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Items from "@/components/Items.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Items,
+  },
+};
 </script>
