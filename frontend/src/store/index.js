917206ee6709014313954e331154cd7015d5ba8d
Michael Van Brummen <michaelvanbrummen@gmail.com>
2022-01-04 14:39:15 +1000

    Added API call to vuex

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,24 @@
-import { createStore } from 'vuex'
+import axios from "axios";
+import { createStore } from "vuex";
+
+const BASE_URL = process.env.API_BASE_URL
+  ? process.env.API_BASE_URL
+  : "http://localhost:8080/v1";
 
 export default createStore({
   state: {
+    topStories: [],
   },
   mutations: {
+    setTopStories(state, topStories) {
+      state.topStories = topStories;
+    },
   },
   actions: {
+    async fetchTopStories({ commit }) {
+      const data = await axios.get(`${BASE_URL}/topstories`);
+      commit("setTopStories", data.data);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,53 +1,22 @@
 <template>
-  <items :itemList="itemList"></items>
+  <items :itemList="this.$store.state.topStories"></items>
 </template>
 
 <script>
 // @ is an alias to /src
 import Items from "@/components/Items.vue";
+import { mapActions } from "vuex";
 
 export default {
   name: "Home",
-  data() {
-    return {
-      itemList: [
-        {
-          id: 29781928,
-          type: "story",
-          time: 1641224975,
-          text: null,
-          by: "eversowhatev",
-          descendants: 22,
-          score: 85,
-          title: "Choose-Your-Own-Adventure AI Dungeon Games",
-          kids: [
-            29787207, 29783808, 29788967, 29789154, 29788247, 29785209,
-            29787789, 29784220, 29783932, 29787188,
-          ],
-          url: "https://www.gwern.net/CYOA",
-          parent: null,
-        },
-        {
-          id: 33333,
-          type: "story",
-          time: 1641224975,
-          text: null,
-          by: "eversowhatev",
-          descendants: 22,
-          score: 85,
-          title: "Testing some VueJs",
-          kids: [
-            29787207, 29783808, 29788967, 29789154, 29788247, 29785209,
-            29787789, 29784220, 29783932, 29787188,
-          ],
-          url: "https://www.gwern.net/CYOA",
-          parent: null,
-        },
-      ],
-    };
-  },
   components: {
     Items,
+  },
+  methods: {
+    ...mapActions(["fetchTopStories"]),
+  },
+  beforeCreate() {
+    this.$store.dispatch("fetchTopStories");
   },
 };
 </script>
