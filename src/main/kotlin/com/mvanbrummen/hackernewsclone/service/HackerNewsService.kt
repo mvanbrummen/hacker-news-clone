b321e0175df0708430673060f2f416d099c3b746
Michael Van Brummen <michaelvanbrummen@gmail.com>
2022-01-04 11:48:43 +1000

    Added coroutines to make parallel http calls

--- src/main/kotlin/com/mvanbrummen/hackernewsclone/service/HackerNewsService.kt
+++ src/main/kotlin/com/mvanbrummen/hackernewsclone/service/HackerNewsService.kt
@@ -2,6 +2,10 @@ package com.mvanbrummen.hackernewsclone.service
 
 import com.mvanbrummen.hackernewsclone.gateway.HackerNewsClient
 import com.mvanbrummen.hackernewsclone.gateway.api.Item
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,7 +17,21 @@ class HackerNewsService(
         val topStoryIds = hackerNewsClient.getTopStories()
             .drop(page * size)
             .take(size)
-        return topStoryIds.map(hackerNewsClient::getItem)
+
+        val items = mutableListOf<Item?>()
+
+        return runBlocking {
+            val result = topStoryIds.map {
+                async(Dispatchers.IO) {
+                    println("Calling item http ${Thread.currentThread().id}")
+                    items.add(hackerNewsClient.getItem(it))
+                }
+            }
+
+            result.awaitAll()
+
+            items
+        }
     }
 
 }
